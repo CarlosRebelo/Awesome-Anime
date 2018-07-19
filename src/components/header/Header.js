34853eb7c892942455fd88ef2f6adb9f1bda08ed
Carlos Rebelo <CarlosRebelo@github.com>
2018-07-19 12:03:01 +0100

    add Header

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+import './Anime.css'
+
+let Header = () => {
+    return(
+        <div className='header'>
+            <div className='anime'>
+                <h1>Awesome Anime</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header
