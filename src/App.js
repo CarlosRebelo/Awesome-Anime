84f06bb25b44168f940f7b394ebeb9cfc8f9e39c
Carlos Rebelo <CarlosRebelo@github.com>
2018-07-19 15:28:36 +0100

    add content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
 import React, { Component } from 'react';
-import './App.css';
 import Header from './components/header/Header'
+import Content from './components/content/Content'
+import './App.css';
+
+function animeList() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = () => {
+    if(xhttp.readyState === 4 && xhttp.status === 200) {
+      const response = JSON.parse(xhttp.response)
+      this.setState({ data: response.data })
+    }
+  };
+  xhttp.open("GET", "https://kitsu.io/api/edge/anime?page[limit]=5&page[offset]=0", true);
+  xhttp.send();
+}
+
+function renderAnime(data) {
+  let AnimeTest = []
+
+  for (let key in data) {
+    AnimeTest[key] = <Content
+      image={data[key].attributes.posterImage.small}
+      title={data[key].attributes.canonicalTitle}
+      description={data[key].attributes.synopsis}
+    />
+  }
+  return AnimeTest
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      data: []
+    };
+  }
+  componentDidMount() {
+
+    animeList.call(this)
+  }
+  
   render() {
     return (
-      <div className="App">
+      
+      <div className='background'>
         <Header/>
+        {renderAnime(this.state.data)}
       </div>
+     
     );
   }
 }
